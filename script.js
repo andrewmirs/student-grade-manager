63ce519d8c2dcd7cf317b3c5f6805a6f7154ff4b
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-10-18 11:04:56 -0700

    started ajax call

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-/* information about jsdocs: 
-* param: http://usejsdoc.org/tags-param.html#examples
-* returns: http://usejsdoc.org/tags-returns.html
-* 
-/**
- * Listen for the document to load and initialize the application
- */
 $(document).ready( initializeApp );
 
-/**
- * Define all global variables here.  
- */
-/***********************
- * student_array - global array to hold student objects
- * @type {Array}
- * example of student_array after input: 
- * student_array = [
- *  { name: 'Jake', course: 'Math', grade: 85 },
- *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
- * ];
- */
+// Globals
 
 var student_array=[];
 
@@ -41,30 +23,14 @@ function initializeApp(){
 function addClickHandlersToElements(){
 }
 
-/***************************************************************************************************
- * handleAddClicked - Event Handler when user clicks the add button
- * @param {object} event  The event object from the click
- * @return: 
-       none
- */
 function handleAddClicked(){
       addStudent();
 }
-/***************************************************************************************************
- * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
- * @param: {undefined} none
- * @returns: {undefined} none
- * @calls: clearAddStudentFormInputs
- */
+
 function handleCancelClick(){
       clearAddStudentFormInputs();
 }
-/***************************************************************************************************
- * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
- * @param {undefined} none
- * @return undefined
- * @calls clearAddStudentFormInputs, updateStudentList
- */
+
 function addStudent(){
       var studentName = $('#studentName').val();
       var studentCourse = $('#course').val();
@@ -78,19 +44,13 @@ function addStudent(){
       clearAddStudentFormInputs();
       updateStudentList( student_array );
 }
-/***************************************************************************************************
- * clearAddStudentForm - clears out the form values based on inputIds variable
- */
+
 function clearAddStudentFormInputs(){
       $('#studentName').val('');
       $('#course').val('');
       $('#studentGrade').val('');
 }
-/***************************************************************************************************
- * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param {object} studentObj a single student object with course, name, and grade inside
- */
+
 function renderStudentOnDom( studentList ){
       $('.student-list tbody').empty();
 
@@ -125,22 +85,14 @@ function removeStudent( student ){
      student_array.splice(index,1);
 }
 
-/***************************************************************************************************
- * updateStudentList - centralized function to update the average and call student list update
- * @param students {array} the array of student objects
- * @returns {undefined} none
- * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
- */
 function updateStudentList( studentList ){
       renderStudentOnDom( studentList );
       renderGradeAverage( calculateGradeAverage(studentList) );
   
 }
-/***************************************************************************************************
- * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
- * @param: {array} students  the array of student objects
- * @returns {number}
- */
+
+// Calculate and Render Student Data
+
 function calculateGradeAverage( studentList ){
       var gradesTotal = 0;
       for (var student=0; student<studentList.length; student++){
@@ -152,12 +104,32 @@ function calculateGradeAverage( studentList ){
       }
       return studentAvg;
 }
-/***************************************************************************************************
- * renderGradeAverage - updates the on-page grade average
- * @param: {number} average    the grade average
- * @returns {undefined} none
- */
 
 function renderGradeAverage( studentAvg ){
       $('.avgGrade').text( studentAvg );
 }
+
+// AJAX Call
+
+var key = {api_key:'BZYxWVMCOE'}
+
+function getData(){
+    console.log('1) getData called from button click');
+    var ajaxConfig = {
+        data: key,
+        dataType: 'json',
+        method: 'POST',
+        url: 'http://s-apis.learningfuze.com/sgt/get',
+        success: function(result) {
+            
+            console.log('2) AJAX Success function called, with the following result:', result);
+
+        }
+    }
+
+    console.log('3) Making AJAX request');
+    $.ajax(ajaxConfig);
+
+    console.log('4) End of getData');
+}
+
