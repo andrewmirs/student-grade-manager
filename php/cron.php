134d29355d3893b5d6bd7dc03f3456cfa777d569
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-02-07 10:44:39 -0800

    changed names in SGT table

--- php/cron.php
+++ php/cron.php
@@ -4,20 +4,21 @@ $conn = mysqli_connect('', '', '', '');
 
 $truncateSQL = "TRUNCATE `students`";
 
-mysqli_query($conn, $truncateSQL);
+mysqli_query($conn, $truncateSQL); 
 
 $insertSQL = "INSERT INTO `students` (`id`, `name`, `course`, `grade`) VALUES
-(69, 'Andre Duparte', 'Biology', 56),
+(69, 'Steven Rogers', 'Biology', 56),
 (71, 'Bruce Wayne', 'Biology', 75),
-(73, 'Demarcus Cousins', 'Biology', 84),
-(76, 'Maynard Guiao', 'Biology', 92),
-(77, 'Tyreek Hill', 'Algebra', 83),
-(78, 'John Tesley', 'Algebra', 78),
-(79, 'Jessica Abrams', 'English', 74),
-(80, 'Sridhar Madala', 'English', 81),
-(81, 'Tony Precios', 'English ', 87),
-(82, 'Andrew Mirshafiee', 'English', 89),
-(86, 'Andy Park', 'English', 97);";
+(73, 'Scott Summers', 'Biology', 84),
+(76, 'Peter Parker', 'Biology', 92),
+(77, 'Cain Marko', 'Algebra', 83),
+(78, 'Andrew Mirshafiee', 'Algebra', 78),
+(79, 'Lynn Hayden', 'English', 74),
+(80, 'Jan Arrah', 'English', 81),
+(81, 'Clark Kent', 'English ', 87),
+(82, 'Orin Curry', 'English', 89),
+(86, 'Anna Marie', 'English', 97);
+";
 
 
 mysqli_query($conn, $insertSQL);
